23969078a13161ee344d5ddd51292b74ef8336bd
davidmorrison1981 <david.morrison@geminises.com>
2016-09-26 21:15:06 +0100

    well I tried to simplify it which hasnt help haha

--- public/app.js
+++ public/app.js
@@ -1,39 +1,69 @@
-var app = function(){
-  addCat("Nizmo","caviar","https://storage.googleapis.com/imgfave/image_cache/1392737882168487.jpg")
-}
+  window.onload = function(){
 
-var addCat = function(){
-  var catClass = createClass();
-  var list = createList();
-  var image = createImage();
-  var cats = document.querySelector('#cats');
-  appendElements(cats, catClass, list, image);
-}
+  //1. Create the parent container and it's class
+  var cats = document.createElement('cats');
+  cats.classList.add('cat')
 
-var createClass = function(){
-  var catClass = document.createElement('ul');
-  catClass.classList.add('ul');
-  return catClass;
-}
+  //2. Create the first child (name)
+  var name = document.createElement('il');
+  name.innerText = "Nizmo ";
 
-var createList = function(){
-  var name = document.createElement('li');
-  var food = document.createElement('li');
-  name.innerText = "Name: " + name;
-  food.innerText = "Food: " + food;
-  return name, food;
-}
+  //3. Create the child inside the name
+  var food = document.createElement('il');
+  food.innerText = "Caviar";
 
-var createImage = function(imageLink){
   var image = document.createElement('img');
-  image.src =imageLink;
-  return image;
+  image.src = "https://storage.googleapis.com/imgfave/image_cache/1392737882168487.jpg";
+
+  //4. Append the food to the name
+  cats.appendChild(food);
+
+  //5. Apprend the name to the cat
+  cats.appendChild(name);
+
+  //6. Add everything to the cats list
+  var cats = document.querySelector('#cats');
+  cats.appendChild(cats);
 }
 
-var appendElements = function(cats, catClass, list, image){
-  catClass.appendChild(image);
- catClass.appendChild(list);
-  cats.appendChild(catClass);
-}
 
-window.onload = app
+
+// var app = function(){
+//   addCat("Nizmo","caviar","https://storage.googleapis.com/imgfave/image_cache/1392737882168487.jpg")
+// }
+
+// var addCat = function(){
+//   var catClass = createClass();
+//   var list = createList();
+//   var image = createImage();
+//   var cats = document.querySelector('#cats');
+//   appendElements(cats, catClass, list, image);
+// }
+
+// var createClass = function(){
+//   var catClass = document.createElement('ul');
+//   catClass.classList.add('ul');
+//   return catClass;
+// }
+
+// var createList = function(){
+//   var name = document.createElement('li');
+//   // var food = document.createElement('li');
+//   name.
+//   // food.innerText = "food: " + food;
+//   return name;
+// }
+
+// var createImage = function(imageLink){
+//   var image = document.createElement('img');z0
+//   image.src =imageLink;
+//   return image;
+// }
+
+// var appendElements = function(cats, catClass, list, image){
+//   catClass.appendChild(image);
+//  catClass.appendChild(list);
+//   cats.appendChild(catClass);
+// }
+
+// window.onload = app
